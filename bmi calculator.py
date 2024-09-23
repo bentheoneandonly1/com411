7b1d5807f762f3a80cc00dacb9bee1bd17658fa9
Ben Chehrazi <0chehb61@solent.ac.uk>
2024-09-23 14:57:00 +0100

    shield health lives

--- bmi calculator.py
+++ bmi calculator.py
@@ -1,6 +1,21 @@
-name = input("Enter your name: ")
-Age = int(input("Enter your age: "))
-height = float(input("Enter your height: "))
-weight = float(input("Enter your weight: "))
-bmi = weight/height**2
-print(name+" you are "+str(Age)+" your BMI is ", str("%.2f" % bmi))
+lives = int(input("Enter number of lives: "))
+energy = int(input("Enter energy: "))
+shield = int(input("Enter shield: "))
+global lives2
+global energy2
+global shield2
+lives2 = ("")
+energy2 = ("")
+shield2 = ("")
+while lives > 0:
+    lives2 += "♥"
+    lives -= 1
+while energy > 0:
+    energy2 += "♦"
+    energy -= 1
+while shield > 0:
+    shield2 += "♦"
+    shield -= 1
+print("Lives:  "+lives2)
+print("Energy:  "+energy2)
+print("Shield:  "+shield2)
