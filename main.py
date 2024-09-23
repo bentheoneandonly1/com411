2b145bda0342d0d3d05f9caf1df7dbcc81161ccb
Ben Chehrazi <0chehb61@solent.ac.uk>
2024-09-23 14:21:13 +0100

    character codes

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # hi how are you
-print("Welcome to COM411!\nIn week 1 we will learn...\n\n...How to use Git and GitHub\n...How to output to the screen\n...How to get user input\n\nI hope you are enjoying the lesson thus far!")
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
