c947c7f5d0329c9a7517829c3ac344a43121420e
Ben Chehrazi <0chehb61@solent.ac.uk>
2024-09-23 14:25:00 +0100

    ascii box

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 # hi how are you
-print("\n Displays a new line")
-print("\t Displays a tab space")
-print("\\ Displays a back slash")
-print("\" Displays a double quote")
-print("\' Displays a single quote")
-print("\n\t\"I am programming!\"")
+print("##########\n#        #\n#        #\n##########")
